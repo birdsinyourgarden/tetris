1242af4ce9c800408171d1cde56f94cec2abbf04
birdsinyourgarden <ruizpaloma9@gmail.com>
2025-02-18 10:44:18 +0100

    logic main

--- main.js
+++ main.js
@@ -4,3 +4,28 @@ const pauseBtn = document.getElementById("pauseButton");
 const scoreElement = document.getElementById("score");
 const highScoreElement = document.getElementById("highScore");
 
+const ROWS = 20, COLS = 10, SIZE = 30;
+// let board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+
+let board = [];
+for (let i = 0; i < ROWS; i++) {
+    board.push(new Array(COLS).fill(0));
+}
+let isPaused = false; 
+let gameInterval;
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0; 
+highScoreElement.textContent = highScore;
+
+const SHAPES = [
+    [[1, 1, 1, 1]], // línea: estoy creando una matriz con una sola fila con cuatro columnas
+    [[1,1], [1,1]], // cuadrado
+    [[0,1,0], [1,1,1]], // T
+    [[1, 1, 1, 1]], // 
+    [[1, 1, 1, 1]], // 
+]
+
+
+
+
+
